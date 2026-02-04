06bab16c109412042f4e0df23a13b316b22f1d96
Edgar Torrez <etorrez10@hotmail.com>
2026-02-03 22:05:55 -0600

    testing 4.6 OpenGL core profile

--- OpenGL/1-GLFW/main.cpp
+++ OpenGL/1-GLFW/main.cpp
@@ -2,5 +2,26 @@
 
 int main ()
 {
+	int initializer = glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "OpenGL GLFW Window", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	
+	glViewport(0, 0, 640, 480);
+
+	while (glfwWindowShouldClose(window) == false)
+	{
+		glfwPollEvents();
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
 	return 0;
 }
